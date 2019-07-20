357db725f5554a0688d75798ea1cbbc50370c558
xiaodoli <xiaodoli@microsoft.com>
2019-07-20 13:02:55 -0700

    "new sln"

---
+++ AcmeCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcmeCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
